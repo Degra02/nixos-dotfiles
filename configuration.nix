c806912f624046dfcdfb92789adb3e9a92382c84
Degra02 <filippodegrandi02@gmail.com>
2024-03-06 15:32:54 +0100

    Fix xkb warnings

--- configuration.nix
+++ configuration.nix
@@ -52,8 +52,8 @@
 
   # Configure keymap in X11
   services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+    xkb.layout = "us";
+    xkb.variant = "";
   };
 
   # Enable CUPS to print documents.
@@ -113,10 +113,10 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
